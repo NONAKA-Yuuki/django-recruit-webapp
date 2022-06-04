3464a26b44a87e58c6813eb574efa71537ada87f
YuukiNonaka <guitiankouki@gmail.com>
2022-06-05 03:33:13 +0900

    initial commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nonakayuuki/Documents/vue_practice/django-drf-tutorial/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
